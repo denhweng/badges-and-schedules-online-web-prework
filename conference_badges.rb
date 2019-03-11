d764b99b85d0368e17ca0cfbc8e0fad639070631
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-11 02:56:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,6 +12,7 @@ end
 def assign_rooms(attendees)
   greeting = []
   attendees.each_with_index { |name, index| "Hello, #{name}! You'll be assigned to room #{index}!" }
+  greeting << attendees.each_with_index
 end
 
 def printer(attendees)
