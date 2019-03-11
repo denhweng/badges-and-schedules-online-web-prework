c453a489a0cbe696561e1b5782bb82254ef5b94e
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-11 03:03:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.map_with_index { |name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!" }
+  attendees.map.with_index { |name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!" }
 end
 
 def printer(attendees)
