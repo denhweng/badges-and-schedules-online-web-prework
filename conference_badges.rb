a153fe4d1617520402bd87dbf0224ec4520aa841
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-11 03:01:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 
 def assign_rooms(attendees)
   greeting = []
-  attendees.each_with_index { |name, index| greeting << "Hello, #{name}! You'll be assigned to room #{index}!" }
+  attendees.each_with_index { |name, index| greeting << "Hello, #{name}! You'll be assigned to room #{index + 1}!" }
   greeting
 end
 
